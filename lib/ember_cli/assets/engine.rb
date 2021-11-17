2d4dc3b555176a34956f888019813fb43f4a6def
Jonathan Tron <jonathan@openhood.com>
2021-11-17 11:24:58 +0100

    Only include `EmberCliRailsAssetsHelper` if `Rails.application.config.action_controller.include_all_helpers` is true

--- lib/ember_cli/assets/engine.rb
+++ lib/ember_cli/assets/engine.rb
@@ -2,7 +2,9 @@ module EmberCli
   module Assets
     class Engine < ::Rails::Engine
       initializer "ember-cli-rails-assets" do
-        ActionController::Base.helper EmberCliRailsAssetsHelper
+        if Rails.application.config.action_controller.include_all_helpers
+          ActionController::Base.helper EmberCliRailsAssetsHelper
+        end
       end
     end
   end
